ab5c7af904ea0b273559a1cc4de6284d98eadb09
Umar Niazi <uhkniazi@gmail.com>
2023-02-01 14:28:19 +0000

    produced some result tables for SNP annotations

--- 11_annotateVariants.R
+++ 11_annotateVariants.R
@@ -47,6 +47,10 @@ oVRall$alleles_as_ambig = NA_character_
 oVRall$RefSNP_id[queryHits(f)] = oGPsnps$RefSNP_id[subjectHits(f)]
 oVRall$alleles_as_ambig[queryHits(f)] = oGPsnps$alleles_as_ambig[subjectHits(f)]
 
+## some stats
+d = data.frame(DPSnps=!is.na(oVRall$RefSNP_id), Patient=oVRall$patient)
+xtabs( ~ DPSnps + Patient, data=d)
+
 ## location of the variants 
 oTxdb = TxDb.Hsapiens.UCSC.hg19.knownGene
 seqlevelsStyle(oTxdb)
@@ -73,9 +77,38 @@ identical(oGRlocations$GENEID, dfSymbol$ENTREZID)
 oGRlocations$SYMBOL = dfSymbol$SYMBOL  
 table(oGRlocations$SYMBOL, oGRlocations$LOCATION)
 
+## create a new VRanges SNPs object and copy
+## the annotations from locations object
+length(unique(oGRlocations$QUERYID)); length(oVRall);
+
+## use the query id mapping variable from locations
+oVRall.locations = oVRall[oGRlocations$QUERYID]
+length(oVRall.locations)
+# sanity check
+table(overlapsAny(oVRall, oVRall.locations))
+length(oVRall.locations); length(oGRlocations);
+oVRall.locations$locationStrand = strand(oGRlocations)
+## merge the metadata
+colnames(mcols(oGRlocations))
+mcols(oVRall.locations) = cbind(mcols(oVRall.locations), mcols(oGRlocations)[,-10])
+
 lResults$gr_locations = oGRlocations
+lResults$vr_with_locations = oVRall.locations
 save(lResults, file='results/lResults_with_VRanges_overlaps_locations.rds')
 
+## get the unique positions i.e. drop the extra transcripts
+length(oVRall.locations)
+i = unique(oVRall.locations$QUERYID)
+i = match(i, oVRall.locations$QUERYID) 
+oVRall.locations.slim = oVRall.locations[i]
+# sanity check
+table(names(oVRall.locations.slim) %in% names(oVRall.locations))
+
+d = data.frame(Location=oVRall.locations.slim$LOCATION, 
+               Patient=oVRall.locations.slim$patient, oVRall.locations.slim$SYMBOL)
+xtabs(~ Location + Patient, data=d)
+
+write.csv(mcols(oVRall.locations.slim), file='results/SNPsWithLocationsAllSamples.csv')
 ### extract the coding locations
 oGRcoding = oGRlocations[oGRlocations$LOCATION == 'coding']
 length(oGRcoding)
@@ -93,6 +126,15 @@ table(oVRall.coding.prot$CONSEQUENCE)
 lResults$vr_coding_proteins = oVRall.coding.prot
 save(lResults, file='results/lResults_with_VRanges_overlaps_locations_coding_consequences.rds')
 
+f = oVRall.coding.prot$CONSEQUENCE == 'nonsynonymous'
+table(f)
+d = data.frame(Patient=oVRall.coding.prot$patient[f], 
+               Gene=oVRall.coding.prot$SYMBOL[f])
+table(d$Patient)
+df = as.data.frame(table(d))
+write.csv(df, file='results/nonsynonymous_frequency.csv')
+
+write.csv(mcols(oVRall.coding.prot[f]), file='results/nonSynonymous_mutations.csv')
 ## get the unique positions i.e. drop the extra transcripts
 length(oVRall.coding)
 i = unique(oVRall.coding.prot$QUERYID)
